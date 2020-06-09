b9b54da9d481b312b33a41e69d4487823029064f
lesa3111 <amael.le.squin@usherbrooke.ca>
2020-06-09 17:25:28 -0400

    Clean code, updated map's colours and legend

--- map_mingDynasty.R
+++ map_mingDynasty.R
@@ -46,36 +46,14 @@ ming_1389 = st_read(dsn = "~/owncloud/database/shapefiles/china/ming_boundaries_
 ming_1433 = st_read(dsn = "~/owncloud/database/shapefiles/china/ming_crossed_1433_kjs")
 currentChina = st_read(dsn = "~/owncloud/database/shapefiles/china/admin_boundary")
 
-## Neighbourhood
-# bhutan = st_read(dsn = "~/owncloud/database/shapefiles/bhutan", layer = "BTN_adm0")
-# india = st_read(dsn = "~/owncloud/database/shapefiles/india", layer = "IND_adm0")
-korea_north = st_read(dsn = "~/owncloud/database/shapefiles/korea_north", layer = "PRK_adm0")
-korea_south = st_read(dsn = "~/owncloud/database/shapefiles/korea_south", layer = "KOR_adm0")
-# laos = st_read(dsn = "~/owncloud/database/shapefiles/laos", layer = "LAO_adm0")
-mongolia = st_read(dsn = "~/owncloud/database/shapefiles/mongolia", layer = "MNG_adm0")
-# myanmar = st_read(dsn = "~/owncloud/database/shapefiles/myanmar", layer = "MMR_adm0")
-# nepal = st_read(dsn = "~/owncloud/database/shapefiles/nepal", layer = "NPL_adm0")
-# vietnam = st_read(dsn = "~/owncloud/database/shapefiles/vietnam", layer = "VNM_adm0")
-
 ## Check crs shapefiles
-# if (!check_crs(ming_1389, ming_1433, currentChina, bhutan, india, korea_north, korea_south, laos, mongolia, myanmar, nepal, vietnam))
-if (!check_crs(ming_1389, ming_1433, currentChina, korea_north, korea_south, mongolia))
+if (!check_crs(ming_1389, ming_1433, currentChina))
 	stop("crs mismatch")
 
 ## Keep polygons
 ming_1389 = st_union(st_geometry(ming_1389))
 ming_1433 = st_union(st_geometry(ming_1433))
 currentChina = st_union(st_geometry(currentChina))
-
-# bhutan = st_union(st_geometry(bhutan))
-# india = st_union(st_geometry(india))
-korea_north = st_union(st_geometry(korea_north))
-korea_south = st_union(st_geometry(korea_south))
-# laos = st_union(st_geometry(laos))
-mongolia = st_union(st_geometry(mongolia))
-# myanmar = st_union(st_geometry(myanmar))
-# nepal = st_union(st_geometry(nepal))
-# vietnam = st_union(st_geometry(vietnam))
 
 #### Plots
 ## Bounding box
@@ -89,33 +67,15 @@ latMin = min(bbox_1389["ymin"], bbox_1433["ymin"], bbox_current["ymin"])
 latMax = max(bbox_1389["ymax"], bbox_1433["ymax"], bbox_current["ymax"])
 
 ## Plot
-jpeg("./MingDynasty.jpg", quality = 100, width = 1080, height = 768)
+jpeg("./MingDynasty.jpg", quality = 100, width = 1080, height = 1080)
 plot(0, pch = "", xlim = c(lonMin, lonMax), ylim = c(latMin, latMax), axes = FALSE,
 	xlab = "", ylab = "", bg = "transparent")
-plot(ming_1389, col = "#FF990022", add = TRUE)
-plot(currentChina, col = NA, lwd = 2, lty = "dotted", add = TRUE)
-plot(korea_north, col = NA, add = TRUE)
-plot(korea_south, col = NA, add = TRUE)
+plot(ming_1433, col = "#FF9900AA", add = TRUE)
+plot(currentChina, col = NA, lwd = 4, lty = "dotted", add = TRUE)
 points(116.383331, y = 39.916668, pch = 15)
 points(118.766670, y = 32.049999, pch = 15)
-text(116.383331, y = 39.916668, labels = "Beijing", pos = 2)
-text(118.766670, y = 32.049999, labels = "Nanjing", pos = 2)
-legend("topleft", legend = c("Ming 1389", "Currently"), col = c("#FF990022", "#000000"),
-	pch = c(15, NA), lty = c(NA, "dotted"), pt.cex = 2, box.lty = 0)
-dev.off()
-
-jpeg("./MingDynasty2.jpg", quality = 100, width = 1080, height = 768)
-plot(0, pch = "", xlim = c(lonMin, lonMax), ylim = c(latMin, latMax), axes = FALSE,
-	xlab = "", ylab = "", bg = "transparent")
-plot(ming_1433, col = "#3366ff22", add = TRUE)
-plot(ming_1389, col = "#FF990022", add = TRUE)
-plot(currentChina, col = NA, lwd = 2, lty = "dotted", add = TRUE)
-plot(korea_north, col = NA, add = TRUE)
-plot(korea_south, col = NA, add = TRUE)
-points(116.383331, y = 39.916668, pch = 15)
-points(118.766670, y = 32.049999, pch = 15)
-text(116.383331, y = 39.916668, labels = "Beijing", pos = 2)
-text(118.766670, y = 32.049999, labels = "Nanjing", pos = 2)
-legend("topleft", legend = c("Ming 1389", "Ming 1433", "Currently"), col = c("#FF9900", "#3366FF", "#000000"),
-	pch = c(15, 15, NA), lty = c(NA, NA, "dotted"), pt.cex = 2, box.lty = 0)
+text(116.383331, y = 39.916668, labels = "Beijing", pos = 2, cex = 2)
+text(118.766670, y = 32.049999, labels = "Nanjing", pos = 2, cex = 2)
+legend("topleft", legend = c("Ming 1433", "Currently"), col = c("#FF9900AA", "#000000"),
+	pch = c(15, NA), lwd = c(NA, 4), lty = c(NA, "dotted"), pt.cex = 6, cex = 2, box.lty = 0)
 dev.off()
